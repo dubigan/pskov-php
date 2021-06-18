c813ac6f60eabc00d8a8c33b1922244920cbe9dc
oak <353991@gmail.com>
2021-06-18 19:07:41 +0300

    onesignal

--- assets/components/App.tsx
+++ assets/components/App.tsx
@@ -11,10 +11,12 @@ const CarDetail = lazy(() => import('./CarDetail'));
 const Dashboard = lazy(() => import('./Dashboard'));
 
 const App = () => {
-  //OneSignal.initialize('4c631ec8-c487-486c-a57f-032561cf353e', {});
+  OneSignal.initialize('4c631ec8-c487-486c-a57f-032561cf353e');
   console.log('OneSignal try init');
   useOneSignalSetup(() => {
     console.log('OneSignal initialized');
+    const userId = OneSignal.getUserId();
+    console.log('App.OneSignal.userId', userId);
     // OneSignal.setEmail(user.Email);
     // OneSignal.setExternalUserId(user.id);
   });
